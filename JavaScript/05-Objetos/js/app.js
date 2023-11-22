25eb3bf0f2da6efe87948c0aa27164da884e86b5
KeiberLLB <leandro1991.9@gmail.com>
2023-11-22 12:18:19 -0500

    anti joda

---
+++ JavaScript/05-Objetos/js/app.js
@@ -0,0 +1,15 @@
+// const coder1 = {
+//   nombre: "Leandro",
+//   edad: 32,
+//   barrio: "Santa Monica",
+// };
+
+// const coder2 = {
+//   nombre: "alejandro",
+//   edad: 23,
+//   barrio: "x",
+// };
+let riwi = { coder1: { nombre: "leandro", edad: 32 } };
+riwi.coder2 = { nombre: "alejandro", edad: 23 };
+riwi.coder2.nombre = "montaña";
+console.log(riwi);
