f5bd7d4ca1fdee0ce1fa8d47ac0f0898f36cf594
KeiberLLB <leandro1991.9@gmail.com>
2023-11-23 13:13:15 -0500

    end class

---
+++ JavaScript/03 - Numeros/js/moodleNumeros.js
@@ -0,0 +1,50 @@
+// 1
+var num = 42;
+var newNum = Number(num);
+
+console.log(newNum);
+
+// 2
+console.log(Number.MAX_VALUE);
+
+// 3
+console.log(Number.MIN_VALUE);
+
+// 4
+console.log(Number.MAX_SAFE_INTEGER);
+
+// 5
+console.log(Number.MIN_SAFE_INTEGER);
+
+// 6
+console.log(Number.isFinite(num));
+
+// 7
+console.log(Number.isInteger(num));
+
+// 8
+console.log(Number.isSafeInteger(num));
+
+// 9
+console.log(num.toExponential(2));
+
+// 10
+console.log(num.toFixed(2));
+
+// 11
+console.log(num.toPrecision(5));
+
+// 12
+let numt = "222.5";
+console.log(Number.parseInt(numt));
+
+// 13
+console.log(Number.parseInt(numt, 9));
+
+// 14
+console.log(Number.parseFloat(numt));
+
+// 15
+console.log(Number.parseFloat(numt, 9));
+
+// 16
