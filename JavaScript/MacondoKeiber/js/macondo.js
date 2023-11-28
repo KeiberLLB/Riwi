16868df7c171b6b5c0031ca22cf3e5b294efdfe2
KeiberLLB <leandro1991.9@gmail.com>
2023-11-28 06:52:51 -0500

    macondo

--- JavaScript/MacondoKeiber/js/macondo.js
+++ JavaScript/MacondoKeiber/js/macondo.js
@@ -1,112 +1,3 @@
-let saldo = 2500000;
-let continuar = true;
-let diasVagaciones = 0;
-let gastos = 0;
-
-//caso 1
-do {
-  const opcion = prompt(`*** Aeropuerto el Dorado ***
-  1. Compra una almojábana con gaseosa son 15.000
-  2. Compra un Subway con gaseosa 23.000
-  3. No compra nada`);
-
-  switch (opcion) {
-    case "1":
-      saldo = saldo - 15000;
-      gastos += 15000;
-      alert(`Su saldo es $ ${saldo}`);
-      continuar = false;
-      break;
-    case "2":
-      saldo = saldo - 23000;
-      gastos += 23000;
-      alert(`Su saldo es $ ${saldo}`);
-      continuar = false;
-      break;
-    default:
-      alert(`Su saldo es $ ${saldo}`);
-      continuar = false;
-      break;
-  }
-} while (continuar);
-
-//caso 2
-
-const altoPermitido = 0.55;
-const largoPermitido = 0.4;
-const anchoPermitido = 0.2;
-let altoOriginal =
-  prompt(`*** Abordaje con maleta de mano ***
-  Ingresa el alto de tu maleta en centimetros`) / 100;
-let largoOriginal =
-  prompt(`*** Abordaje con maleta de mano ***
-  Ingresa el largo de tu maleta en centimetros`) / 100;
-let anchoOriginal =
-  prompt(`*** Abordaje con maleta de mano ***
-  Ingrese el ancho de tu maleta en centimetros`) / 100;
-alert(`Es un poco mas grande del tamaño normal.
-Retira las prendas que consideres necesarias para obtener las dimensiones adecuadas.
-¡Retirando prendas ... !`);
-let factorAlto = altoPermitido / altoOriginal;
-let factorLargo = largoPermitido / largoOriginal;
-let factorAncho = anchoPermitido / anchoOriginal;
-const factorReduccion = Math.min(factorAlto, factorLargo, factorAncho);
-let nuevoAlto = altoOriginal * factorReduccion;
-let nuevoLargo = largoOriginal * factorReduccion;
-let nuevoAncho = anchoOriginal * factorReduccion;
-alert(
-  `Tu maleta es ${nuevoAlto} cm de alto, ${nuevoLargo} cm de largo y ${nuevoAncho} cm de ancho`
-);
-alert(`Las nuevas dimensiones cumplen con los estanderes establecidos`);
-
-//caso 3
-
-alert(`!!!Bienvenido a Medellin!!!
-Recordatorio!: debes estar por lo menos 20 minutos antes en sala de espera para abortar tu proximo vuelo`);
-alert(`Confirma tu Reserva`);
-alert(`Buscando red WiFi...`);
-const redWiFi = `Nuenva red disponible: 
-ElPassEs: 01110010_01101001_01110111_01101001`;
-alert(`${redWiFi}`);
-alert(`***Hacking WiFi Passwords***`);
-let claveWiFi = redWiFi.slice(34, 69);
-claveWiFi = claveWiFi.split("_");
-let contraseña = "";
-for (const i of claveWiFi) {
-  const decimal = parseInt(i, 2);
-  const letra = String.fromCharCode(decimal);
-  contraseña += letra;
-}
-alert(`Clave WiFi obtenida: ${contraseña} 
-Reserva Confirmada con exito`);
-saldo = saldo - 50000;
-gastos += 50000;
-alert(`Se ha generado un cobro de $ ${50000}
-Concepto: Uso WiFi/hora`);
-alert(`Tu saldo actual es de $ ${saldo}`);
-
-//caso 4
-alert(`¡Bienvenido a Macondo!`);
-
-do {
-  continuar = true;
-  const opcion = prompt(`*** Herramienta Comunicacion Macondo ***
-  1. Traductor.
-  2. Salir`);
-
-  switch (opcion) {
-    case "1":
-      let frase = prompt("Traducir: ");
-      alert(`Traduciendo ...`);
-      frase = frase.replace(/[aeou]/g, "i");
-      alert(frase);
-      break;
-
-    default:
-      continuar = false;
-      break;
-  }
-} while (continuar);
 function juegoTaxi() {
   do {
     continuar = true;
@@ -171,6 +62,117 @@ function juegoTaxi() {
     }
   } while (continuar);
 }
+function WiFi() {
+  alert(`Buscando red WiFi...`);
+  const redWiFi = `Nuenva red disponible: 
+ElPassEs: 01110010_01101001_01110111_01101001`;
+  alert(`${redWiFi}`);
+  alert(`***Hacking WiFi Passwords***`);
+  let claveWiFi = redWiFi.slice(34, 69);
+  claveWiFi = claveWiFi.split("_");
+  let contraseña = "";
+  for (const i of claveWiFi) {
+    const decimal = parseInt(i, 2);
+    const letra = String.fromCharCode(decimal);
+    contraseña += letra;
+  }
+  alert(`Clave WiFi obtenida: ${contraseña} 
+Reserva Confirmada con exito`);
+  saldo -= 50000;
+  gastos += 50000;
+  alert(`Se ha generado un cobro de $ ${50000}
+Concepto: Uso WiFi/hora`);
+  alert(`Tu saldo actual es de $ ${saldo}`);
+}
+let saldo = 2500000;
+let continuar = true;
+let diasVagaciones = 0;
+let gastos = 0;
+
+//caso 1
+do {
+  const opcion = prompt(`*** Aeropuerto el Dorado ***
+  1. Compra una almojábana con gaseosa son 15.000
+  2. Compra un Subway con gaseosa 23.000
+  3. No compra nada`);
+
+  switch (opcion) {
+    case "1":
+      saldo = saldo - 15000;
+      gastos += 15000;
+      alert(`Su saldo es $ ${saldo}`);
+      continuar = false;
+      break;
+    case "2":
+      saldo = saldo - 23000;
+      gastos += 23000;
+      alert(`Su saldo es $ ${saldo}`);
+      continuar = false;
+      break;
+    default:
+      alert(`Su saldo es $ ${saldo}`);
+      continuar = false;
+      break;
+  }
+} while (continuar);
+
+//caso 2
+
+const altoPermitido = 0.55;
+const largoPermitido = 0.4;
+const anchoPermitido = 0.2;
+let altoOriginal =
+  prompt(`*** Abordaje con maleta de mano ***
+  Ingresa el alto de tu maleta en centimetros`) / 100;
+let largoOriginal =
+  prompt(`*** Abordaje con maleta de mano ***
+  Ingresa el largo de tu maleta en centimetros`) / 100;
+let anchoOriginal =
+  prompt(`*** Abordaje con maleta de mano ***
+  Ingrese el ancho de tu maleta en centimetros`) / 100;
+alert(`Es un poco mas grande del tamaño normal.
+Retira las prendas que consideres necesarias para obtener las dimensiones adecuadas.
+¡Retirando prendas ... !`);
+let factorAlto = altoPermitido / altoOriginal;
+let factorLargo = largoPermitido / largoOriginal;
+let factorAncho = anchoPermitido / anchoOriginal;
+const factorReduccion = Math.min(factorAlto, factorLargo, factorAncho);
+let nuevoAlto = altoOriginal * factorReduccion;
+let nuevoLargo = largoOriginal * factorReduccion;
+let nuevoAncho = anchoOriginal * factorReduccion;
+alert(
+  `Tu maleta es ${nuevoAlto} cm de alto, ${nuevoLargo} cm de largo y ${nuevoAncho} cm de ancho`
+);
+alert(`Las nuevas dimensiones cumplen con los estanderes establecidos`);
+
+//caso 3
+alert(`!!!Bienvenido a Medellin!!!
+Recordatorio!: debes estar por lo menos 20 minutos antes en sala de espera para abortar tu proximo vuelo`);
+alert(`Confirma tu Reserva`);
+WiFi();
+//caso 4
+alert(`¡Bienvenido a Macondo!`);
+
+do {
+  continuar = true;
+  const opcion = prompt(`*** Herramienta Comunicacion Macondo ***
+  1. Traductor.
+  2. Salir`);
+
+  switch (opcion) {
+    case "1":
+      let frase = prompt("Traducir: ");
+      alert(`Traduciendo ...`);
+      frase = frase.replace(/[aeou]/g, "i");
+      alert(frase);
+      break;
+
+    default:
+      continuar = false;
+      break;
+  }
+} while (continuar);
+
 //caso 5
 alert(`Llegaste al Hotel:`);
 alert(`Taxista: Siriin $ 300 000 pir il sirvicii`);
@@ -271,7 +273,7 @@ do {
         diasVagaciones++;
       }
       break;
-    case "3":
+    case "4":
       alert(`Elegiste ${vestuario[opcion - 1]} `);
       op = prompt(`DIA DE ACTIVIDADES EN EL HOTEL !!!
         1. Bingo
@@ -301,7 +303,7 @@ do {
   }
 } while (continuar && vivo && diasVagaciones < 4);
 
-gastos += diasVagaciones + costoDiaH;
+gastos += diasVagaciones * costoDiaH;
 saldo -= gastos;
 
 if (casino == true) {
@@ -316,6 +318,6 @@ if (casino == true) {
   Solo pudiste Vagacionar ${diasVagaciones} dias`);
 } else {
   alert(`Saldo Final $ ${saldo}
-  Dias vacacionados: ${diasVagaciones}
+  Dias vagacionados: ${diasVagaciones}
   Gastos Totales ${gastos}`);
 }
