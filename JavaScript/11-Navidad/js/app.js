2834b81dd68564b9a0933d5631301fdbde96e4f1
KeiberLLB <leandro1991.9@gmail.com>
2023-12-11 13:21:36 -0500

    end class

--- JavaScript/11-Navidad/js/app.js
+++ JavaScript/11-Navidad/js/app.js
@@ -1,4 +1,7 @@
 const contNieve = document.querySelector(".nieve");
+const audioNavidad = document.querySelector("#musica-navideña");
+audioNavidad.play();
+audioNavidad.loop = true;
 contNieve.innerHTML = "";
 
 function generarNieve() {
@@ -16,6 +19,39 @@ function generarNieve2() {
 function random(inicio, final) {
   return Math.floor(Math.random() * (final - inicio + 1) + inicio);
 }
+
+function timerNavidad() {
+  //obtener fecha actual
+  const fechaActual = new Date();
+  //definir fecha objetivo
+  const fechaObjetivo = new Date(fechaActual.getFullYear(), 11, 25); //el mes 11 representa diciembre
+  //verificar si la fehca objetivo ya paso en este año
+  if (fechaActual.getMonth() === 11 && fechaActual.getDate() > 25) {
+    fechaObjetivo.setFullYear(fechaObjetivo.getFullYear() + 1);
+  }
+  //calcular la diferencia entre las dos fechas
+  let diferenciaMs = fechaObjetivo - fechaActual;
+
+  //convertir los ms a dias horas minutos segundos
+  let diasRestantes = Math.floor(diferenciaMs / (1000 * 60 * 60 * 24));
+  let horasRestantes = Math.floor(
+    (diferenciaMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  let minutosRestantes = Math.floor(
+    (diferenciaMs % (1000 * 60 * 60)) / (1000 * 60)
+  );
+  let segundosRestantes = Math.floor((diferenciaMs % (1000 * 60)) / 1000);
+
+  const diasHTML = document.getElementById("dia-timer");
+  const horaHTML = document.getElementById("hora-timer");
+  const minutosHTML = document.getElementById("minutos-timer");
+  const segundosHTML = document.getElementById("segundos-timer");
+  diasHTML.innerHTML = diasRestantes.toString().padStart(2, "0");
+  horaHTML.innerHTML = horasRestantes.toString().padStart(2, "0");
+  minutosHTML.innerHTML = minutosRestantes.toString().padStart(2, "0");
+  segundosHTML.innerHTML = segundosRestantes.toString().padStart(2, "0");
+}
+setInterval(timerNavidad, 1000);
 generarNieve();
 generarNieve2();
 
