98b409fd42d68dd9f9983686cb186985bc66e676
KeiberLLB <leandro1991.9@gmail.com>
2024-01-24 07:39:38 -0500

    end review

--- JavaScript/SPACEX/js/app.js
+++ JavaScript/SPACEX/js/app.js
@@ -1,3 +1,15 @@
-import { renderMain } from "./function.js";
-
+import { renderMain, verMas } from "./function.js";
+const container = document.querySelector(".container");
 renderMain();
+
+container.addEventListener("click", (event) => {
+  event.preventDefault();
+  if (event.target.classList.contains("btn-primary")) {
+    const id = event.target.getAttribute("data-id");
+    verMas(id);
+  }
+
+  // if (event.target.classList.contains("bx-arrow-back")) {
+  //   callToApi(inputTitle.value);
+  // }
+});
--- JavaScript/SPACEX/js/function.js
+++ JavaScript/SPACEX/js/function.js
@@ -1,5 +1,48 @@
 const container = document.querySelector(".container");
 
-async function renderMain() {
-  cleanBody();
+export async function renderMain() {
+  // cleanBody();
+  const URL = "https://api.spacexdata.com/v3/launches";
+  const response = await fetch(URL);
+  const data = await response.json();
+  if (!data) {
+    const titleError = document.createElement("h2");
+    titleError.textContent = "No se encontraron mision";
+    titleError.classList.add("alert");
+    container.appendChild(titleError);
+  }
+  data.forEach((mision) => {
+    container.innerHTML += `
+    <div
+      class="card d-flex flex-column align-items-center text-center col-3"
+      style="width: 18rem"
+    >
+      <img
+        src="${mision.links.mission_patch_small}"
+        class="card-img-top"
+        alt="..."
+      />
+      <div class="card-body">
+        <h5 class="card-title">${mision.mission_name}</h5>
+        <p class="card-text">${mision.launch_year}</p>
+        
+        <button
+          type="button"
+          data-id="${mision.flight_number}" 
+          class="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Ver Info
+        </button>
+      </div>
+    </div>`;
+  });
 }
+
+
+
+export function verMas(id) {
+  console.log(id);
+}
+
