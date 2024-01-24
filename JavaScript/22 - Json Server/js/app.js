ee54ba7ef233933408c29cc881312e7baa699f1a
KeiberLLB <leandro1991.9@gmail.com>
2024-01-24 13:35:37 -0500

    end class

--- JavaScript/22 - Json Server/js/app.js
+++ JavaScript/22 - Json Server/js/app.js
@@ -5,7 +5,8 @@ const ageUser = document.querySelector("#user-age");
 const idUser = document.querySelector("#user-id");
 const form = document.querySelector("#form");
 const tbody = document.querySelector("#tbody");
-
+const users = getUsers();
+let cache;
 // eventos
 document.addEventListener("DOMContentLoaded", () => {
   getUsers();
@@ -14,8 +15,8 @@ document.addEventListener("DOMContentLoaded", () => {
 tbody.addEventListener("click", (event) => {
   event.preventDefault();
   if (event.target.classList.contains("delete")) {
-    const id = event.target.getAttribute("data-id");
-    eliminar(id);
+    const index = event.target.getAttribute("data-id");
+    eliminar(index);
   }
   if (event.target.classList.contains("edit")) {
     const id = event.target.getAttribute("data-id");
@@ -25,7 +26,11 @@ tbody.addEventListener("click", (event) => {
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  createUser();
+  if (cache === undefined) {
+    createUser();
+  } else {
+    updateUser(cache);
+  }
 });
 
 async function createUser() {
@@ -35,6 +40,7 @@ async function createUser() {
   //PUT -> ACTUALIZAR
   //DELETE -> ELIMINAR
   //PATCH -> ACTUALIZAR PARCIAL
+
   const user = {
     name: nameUser.value,
     age: ageUser.value,
@@ -52,20 +58,20 @@ async function getUsers() {
   //fetch trae por defecto GET
   const response = await fetch(`${URLBase}/users`);
   const data = await response.json();
-  console.log(data);
+
   renderUser(data);
+  return data;
 }
 
 function renderUser(users) {
-  console.log(users);
   cleanTbody();
   users.forEach((user) => {
     const tr = document.createElement("tr");
     tr.innerHTML += `<td>${user.id}</td>
     <th>${user.name}</th>
     <th>${user.age}</th>
-    <td><button class="edit" data-id="${user.id}">Editar</button></td>
-    <td><button class="delete" data-id="${user.id}">Eliminar</button></td>
+    <td><button class="edit btn btn-primary" data-id="${user.id}">Editar</button></td>
+    <td><button class="delete btn btn-danger" data-id="${user.id}">Eliminar</button></td>
     `;
     tbody.appendChild(tr);
   });
@@ -75,4 +81,38 @@ function cleanTbody() {
   while (tbody.firstChild) {
     tbody.removeChild(tbody.firstChild);
   }
+}
+
+async function editar(id) {
+  const data = await getUsers();
+  const u = data.find((user) => user.id === id);
+  nameUser.value = u.name;
+  ageUser.value = u.age;
+  cache = id;
+}
+
+async function updateUser(id) {
+  // const data = await getUsers();
+  // const u = data.find((user) => user.id === id);
+  const user = {
+    name: nameUser.value,
+    age: ageUser.value,
+  };
+  const response = await fetch(`${URLBase}/users/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+  cache = undefined;
+}
+
+async function eliminar(id) {
+  const response = await fetch(`${URLBase}/users/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
