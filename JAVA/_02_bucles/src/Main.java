ea03c666313b779ba7d8399e67c8fbe3594508b5
KeiberLLB <leandro1991.9@gmail.com>
2024-03-05 13:04:57 -0500

    java Ejercicios

--- JAVA/_02_bucles/src/Main.java
+++ JAVA/_02_bucles/src/Main.java
@@ -6,34 +6,34 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         /*Contador Simple: Escribe un programa que utilice un bucle for para contar del 1 al 10 e
-        imprimir cada número en la consola.
+        imprimir cada número en la consola.*/
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
-        }*/
+        }
 
         /*Suma de Números: Utiliza un bucle while para sumar los números del 1 al 100 e imprimir
-        el resultado.
+        el resultado.*/
         int contador = 0;
         int sumaTotal = 0;
         while (contador <= 100) {
             sumaTotal += contador;
             contador++;
         }
-        System.out.println(sumaTotal);*/
+        System.out.println(sumaTotal);
 
         /*Tabla de Multiplicar: Utiliza un bucle for anidado para imprimir la tabla de multiplicar
-        del 1 al 10.
+        del 1 al 10.*/
         for (int t = 1; t <= 10; t++) {
             System.out.println("\n" + "Tabla del " + t + "\n");
             for (int i = 1; i <= 10; i++) {
                 System.out.println(t + " x " + i + " = " + (t * i));
             }
-        }*/
+        }
 
         /*Sistema de Menú Interactivo: Desarrolla un sistema de menú interactivo que muestre
         diferentes opciones al usuario (por ejemplo, 1. Ver saldo 2. Depositar dinero 3. Retirar
         dinero 4. Salir). Utiliza un bucle para permitir al usuario interactuar con el menú hasta que
-        elija salir.
+        elija salir.*/
         String option;
         double saldo = 0;
         do {
@@ -71,12 +71,12 @@ public class Main {
                     }
                     break;
             }
-        } while (!option.equals("4"));*/
+        } while (!option.equals("4"));
 
         /*Validación de Entrada de Usuario: Escribe un programa que solicite al usuario que
         ingrese su edad. Si el usuario ingresa un valor que no es un número válido o está fuera de
         un rango razonable (por ejemplo, 0-120), el programa debe pedirle al usuario que intente de
-        nuevo. Utiliza un bucle para seguir solicitando la entrada hasta que sea válida.
+        nuevo. Utiliza un bucle para seguir solicitando la entrada hasta que sea válida.*/
         boolean verificacion = false;
         do {
             try {
@@ -90,10 +90,10 @@ public class Main {
             } catch (Exception e) {
                 JOptionPane.showMessageDialog(null, "La edad ingresada no es valida");
             }
-        } while (!verificacion);*/
+        } while (!verificacion);
 
         /*Verificar un Número Primo: Escribe un programa que utilice un bucle para verificar si un
-        número dado es primo o no.
+        número dado es primo o no.*/
         double numero = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresa el numero a verificar si es primo: "));
 
         if (numero == 0 || numero == 1 || numero == 4) {
@@ -113,7 +113,7 @@ public class Main {
             } else {
                 JOptionPane.showMessageDialog(null, "El numero " + numero + " no es primo");
             }
-        }*/
+        }
 
         /*Juego de Adivinanzas: Crea un pequeño juego de adivinanzas donde el usuario debe
         adivinar un número generado aleatoriamente. Utiliza un bucle do-while para permitir al
@@ -141,5 +141,21 @@ public class Main {
         } else {
             JOptionPane.showMessageDialog(null, "No tienes mas intentos! Perdiste");
         }
+
+        /*Cálculo de Interés Compuesto: Crea un programa que calcule el crecimiento de una
+        inversión bajo un esquema de interés compuesto. El usuario debe poder ingresar el capital
+        inicial, la tasa de interés anual y el número de años. Utiliza un bucle para calcular y mostrar
+        el saldo de la inversión al final de cada año.*/
+        double inversion = Double.parseDouble(JOptionPane.showInputDialog(null,"Ingresa la inversion que deseas realizar : $"));
+        double interes = Double.parseDouble(JOptionPane.showInputDialog(null,"Ingresa la tasa de interes anual: "));
+        double años = Double.parseDouble(JOptionPane.showInputDialog(null, "Por cuantos años?"));
+        int counter=0;
+
+        for (int i =0;i<=años;i++){
+            inversion = inversion + inversion*(interes/100);
+            System.out.println(inversion);
+        }
+        JOptionPane.showMessageDialog(null,"Saldo final: $"+inversion);
+
     }
 }
