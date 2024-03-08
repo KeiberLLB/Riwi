1e74c021139beab365843252d20b35e3ac7f574a
KeiberLLB <leandro1991.9@gmail.com>
2024-03-08 10:21:19 -0500

    banco Sebas

---
+++ JAVA/_04_POO/src/Banco/entidad/Usuario.java
@@ -0,0 +1,11 @@
+package Banco.entidad;
+
+import java.util.ArrayList;
+
+public class Usuario {
+    private ArrayList<Usuario> usuarios;
+    private int id;
+    private int idCuenta;
+    name, contraseña
+
+}
