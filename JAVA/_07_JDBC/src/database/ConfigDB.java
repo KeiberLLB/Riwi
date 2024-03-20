0e8a98f7967ae0433ebeffd332fc57782437832e
KeiberLLB <leandro1991.9@gmail.com>
2024-03-20 09:21:33 -0500

    prueba

--- JAVA/_07_JDBC/src/database/ConfigDB.java
+++ JAVA/_07_JDBC/src/database/ConfigDB.java
@@ -4,22 +4,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
 public class ConfigDB {
     //Variable que va a contener el estado de la conexión
     static Connection objConnection = null;
-
     //método par abrir la conexión entre java y la base de datos
     public static Connection openConnection() {
         try {
             //class forName permite obtener o implementar el driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-
             //Creamos variables con nuestra credenciales de la base de datos
             String url = "jdbc:mysql://localhost:3306/_01_jdbc";
             String user = "root";
             String password = "Rlwl2023.";
-
             //Establecemos la conexión
             objConnection = (Connection) DriverManager.getConnection(url, user, password);
             System.out.println("Successful connection!");
@@ -36,7 +32,6 @@ public class ConfigDB {
             System.out.println("Connection Finished");
         } catch (SQLException e) {
             System.out.println("Error: " + e.getMessage());
-
         }
     }
 }
